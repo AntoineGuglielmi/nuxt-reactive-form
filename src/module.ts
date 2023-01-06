9eb7c427a69734ce586133746f29f2da128d0c6f
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-01-06 01:10:36 +0100

    Form reactivity + validation ok

--- src/module.ts
+++ src/module.ts
@@ -4,7 +4,7 @@ import {
   addPlugin,
   createResolver,
   addImports
-} from '@nuxt/kit';
+} from '@nuxt/kit'
 
 export interface ModuleOptions {
   addPlugin: boolean
@@ -25,10 +25,48 @@ export default defineNuxtModule<ModuleOptions>({
     if (options.addPlugin) {
       addPlugin(resolve(runtimeDir, 'plugin'))
     }
-    addImports({
-      from: resolve(runtimeDir, 'composables/useForm'),
-      name: 'useForm',
-      as: 'useForm'
-    })
+
+    const rules = [
+      'required',
+      'maxLength',
+      'minLength',
+      'onlyLetters',
+      'onlyNumbers'
+    ]
+
+    const types = [
+      'IStateInit',
+      'IState',
+      'IErrorMessages',
+      'TUseForm',
+      'IStateReset',
+      'IStateInitPackage',
+      'TValidationRule',
+      'TValidationRuleWrapper',
+      'IValidationRuleParams',
+      'IValidationRulePayload'
+    ]
+
+    addImports([
+      {
+        from: resolve(runtimeDir, 'composables/useForm'),
+        name: 'useForm',
+        as: 'useForm'
+      },
+      ...rules.map((rule: string) => {
+        return {
+          name: rule,
+          as: rule,
+          from: resolve(runtimeDir, 'utils/useFormValidationRules')
+        }
+      }),
+      ...types.map((type: string) => {
+        return {
+          name: type,
+          as: type,
+          from: resolve(runtimeDir, 'types/useFormTypes')
+        }
+      })
+    ])
   }
 })
---
+++ src/runtime/utils/useFormValidationRules.ts
@@ -0,0 +1,60 @@
+import { TValidationRuleWrapper, TValidationRule, IValidationRuleParams, IValidationRulePayload } from '../types/useFormTypes'
+
+export const required: TValidationRuleWrapper = (params: IValidationRuleParams = {}): TValidationRule => {
+  const defaultedParams = {
+    message: 'This field is not valid',
+    ...params
+  }
+  return (payload: IValidationRulePayload) => {
+    const { value } = payload
+    return (value !== null && value !== '' && value !== undefined) || defaultedParams.message
+  }
+}
+
+export const maxLength: TValidationRuleWrapper = (params: IValidationRuleParams = {}): TValidationRule => {
+  const defaultedParams = {
+    message: 'This field is not valid',
+    max: -1,
+    ...params
+  }
+  return (payload: IValidationRulePayload) => {
+    const { value } = payload
+    return value.length <= defaultedParams.max || defaultedParams.max === -1 || defaultedParams.message
+  }
+}
+
+export const minLength: TValidationRuleWrapper = (params: IValidationRuleParams = {}): TValidationRule => {
+  const defaultedParams = {
+    message: 'This field is not valid',
+    min: -1,
+    ...params
+  }
+  return (payload: IValidationRulePayload) => {
+    const { value } = payload
+    return value.length <= defaultedParams.min || defaultedParams.min === -1 || defaultedParams.message
+  }
+}
+
+export const onlyLetters: TValidationRuleWrapper = (params: IValidationRuleParams = {}): TValidationRule => {
+  const defaultedParams = {
+    message: 'This field is not valid',
+    ...params
+  }
+  return (payload: IValidationRulePayload) => {
+    const { value } = payload
+    const regexp = /^[A-Za-zÀ-ÖØ-öø-ÿ ]*$/
+    return (regexp.test(value)) || defaultedParams.message
+  }
+}
+
+export const onlyNumbers: TValidationRuleWrapper = (params: IValidationRuleParams = {}): TValidationRule => {
+  const defaultedParams = {
+    message: 'This field is not valid',
+    ...params
+  }
+  return (payload: IValidationRulePayload) => {
+    const { value } = payload
+    const regexp = /^[0-9]*$/
+    return (regexp.test(value)) || defaultedParams.message
+  }
+}
